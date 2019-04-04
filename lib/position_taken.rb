c891807646cbfd8bfa793a04238bcc2f4e075253
Nathaniel Stiers <nvstiers@gmail.com>
2019-04-04 03:58:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  board[index] == " " || ""
-  return false
+  if board[index] == " " || ""
+    return false
+  end
 
 end
